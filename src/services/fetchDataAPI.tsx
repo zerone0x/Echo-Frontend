5a710eb5ba379fdf4cf93b9a4b3c07ced64c7519
zerone0x <soberzml2333@gmail.com>
2024-06-10 04:31:27 +0800

    Add IoIosMore icon and fetch feeds on Home page

--- src/services/fetchDataAPI.tsx
+++ src/services/fetchDataAPI.tsx
@@ -6,26 +6,42 @@ export const authUserRegister = async () => {
 };
 
 export const authUserLogin = async (email, password) => {
-  const response = await client.post("auth/login", {
+  const { data } = await client.post("auth/login", {
     email: email,
     password: password,
   });
-  return response.user.email;
+  return data.results.user;
 };
 
 export const authUserSignUp = async (email, password, name) => {
-  const response = await client.post("auth/register", {
+  const { data } = await client.post("auth/register", {
     email: email,
     password: password,
     name: name,
   });
-  return response.user.name;
+  return data.results.user;
 };
-// export const authUserGoogle = async () => {
-//   const { data } = await client.get('auth/google')
-//   return data.results
-// }
 
-// export const authUserGithub = async () => {
-//   await client.get('auth/github')
-// }
+export const CreateFeed = async (content, user) => {
+  const { data } = await client.post("feeds", {
+    content: content,
+    user: user,
+  });
+  return data.results;
+};
+
+export const GetAllFeeds = async () => {
+  const { data } = await client.get("feeds");
+  console.log(data.results);
+  return data.results;
+};
+
+export const DeleteFeed = async (feedId: string) => {
+  const { data } = await client.delete(`feeds/${feedId}`);
+  return data.results;
+};
+
+export const GetFeedById = async (feedId: string) => {
+  const { data } = await client.get(`feeds/${feedId}`);
+  return data.results;
+};
