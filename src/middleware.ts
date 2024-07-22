7050c5b15a1dce3a6267aa4320432c632e0b783c
zerone0x <soberzml2333@gmail.com>
2024-07-23 00:16:00 +0800

    magic

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,17 @@ import type { NextRequest } from "next/server";
 import { verifyJwtToken } from "./app/_utils/auth";
 
 export async function middleware(request: NextRequest) {
-  console.log(request);
+  console.log('request', request);
   // let token = request.cookies.get("token")?.value;
-  let token = request.cookies.get("token")?.value;
+  let token = request.cookies.get("_vercel_jwt")?.value;
   console.log('token',token);
 
-  if (token) {
-    if (token.startsWith("s:")) {
-      token = token.slice(2);
-    }
-    token = token.split(".").slice(0, 3).join(".");
-  }
+  // if (token) {
+  //   if (token.startsWith("s:")) {
+  //     token = token.slice(2);
+  //   }
+  //   token = token.split(".").slice(0, 3).join(".");
+  // }
   console.log('after token', token);
   
 
