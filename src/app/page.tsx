6e3053bbfa1562d6bfba8e3cb4316d990d35138a
zerone0x <soberzml2333@gmail.com>
2024-08-14 22:58:22 +0800

    Update page title and metadata description for Echo app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,24 @@ import Welcome from "./_components/Welcome";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Welcome to Echo!",
+  title: "Echo Social App",
+  description:
+    "Stay updated with news from your friends! Create a profile, bookmark favorites, and receive notifications. Interact with others and share your own updates!",
+  openGraph: {
+    title: "Echo Social App",
+    description:
+      "Stay updated with news from your friends! Create a profile, bookmark favorites, and receive notifications. Interact with others and share your own updates!",
+    url: "https://echoloop.netlify.app/",
+    siteName: "Echo your thoughts",
+    images: [
+      {
+        url: "https://res.cloudinary.com/soberzml/image/upload/v1723647221/trinedev/echohome_xndxks.png",
+        width: 1200,
+        height: 630,
+        alt: "Open Graph Image",
+      },
+    ],
+  },
 };
 
 function App() {
